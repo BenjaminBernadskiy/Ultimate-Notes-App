f7e7fec0e7aca96db48f6ff735cc54b4fd57a556
Ben.Bernadskiy <90880517+bluepuppy9123@users.noreply.github.com>
2023-09-13 22:14:04 -0400

    added ids and more

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pygame
 import sys
-import json
+import random
 import time
 
 # Initialize Pygame
 pygame.init()
 
 # Constants
-WIDTH, HEIGHT = 800, 600
+# get height and width
+WIDTH = 800
+HEIGHT = 600
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 FONT_SIZE = 20
@@ -15,7 +17,7 @@ FONT_COLOR = (255, 255, 255)
 
 # Create the screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
-pygame.display.set_caption("Ultimate Notes")
+pygame.display.set_caption("Ben's Notes App")
 
 # Initialize font
 font = pygame.font.Font(None, FONT_SIZE)
@@ -23,6 +25,7 @@ title_font = pygame.font.Font(None, 35)
 
 running = True
 while running:
+    mouse_x, mouse_y = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -31,30 +34,77 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             pass
     # Clear the screen
-    screen.fill((50, 50, 170))
+    screen.fill(BLACK)
 
     # create a loading animation of a notebook opening
 
     # create notepad and have boxes with titles of notes on it and make them clickable to go into notes
     # screen.blit(notepad will go here)
     # load all note names
+    title_and_location = []  # Initialize title_and_location as an empty tuple
+    ids = []
     with open("Ultimate-Notes-App\data.txt", 'r') as file:
         note_titles = []
+        x = -1
         for line in file:
+            x += 40
             # Process each line here
             # This will print each line without the trailing newline character
-            if len(line.strip()) > 20:
-                note_titles.append(line.strip()[0:20] + '...')
+            # if there is a ((())) that means there is an assigned title; otherwise, just use the notes
+            if line.strip().__contains__('((('):
+                note_titles.append(line.strip().split(')))')[0][3:])
+                # create id for each note
+                id = ''
+                for i in range(10):
+                    id += str(random.randint(0, 9))
+                ids.append(id)
+                # HOW ID WORKS ------------------------------------- the ids are created in order so if you pull out an id from the list
+                # and find its index it will be linked to the correct note since it will be on the same line
+                # Append the new tuple to title_and_location
+                title_and_location.append(
+                    f'{x}:{id}')
             else:
-                note_titles.append(line.strip())
-
-        # print(note_titles)
+                if len(line.strip()) > 20:
+                    note_titles.append(line.strip()[0:15] + '...')
+                    # create id for each note
+                    id = ''
+                    for i in range(10):
+                        id += str(random.randint(0, 9))
+                    ids.append(id)
+                    # Append the new tuple to title_and_location
+                    title_and_location.append(
+                        f'{x}:{id}')
+                else:
+                    note_titles.append(line.strip())
+                    # create id for each note
+                    id = ''
+                    for i in range(10):
+                        id += str(random.randint(0, 9))
+                    ids.append(id)
+                    # Append the new tuple to title_and_location
+                    title_and_location.append(
+                        f'{x}:{id}')
     i = -1
+    heights = []
+    titles = []
+    for tt in title_and_location:
+        heights.append(tt.split(':')[0])
+        titles.append(tt.split(':')[1])
+    # check if curser is on one of the boxes and then link it with an id
+    for height in heights:
+        if mouse_x >= (WIDTH // 2) - 125 and mouse_x <= (WIDTH // 2) + 80 and mouse_y >= (HEIGHT // 2) + int(height) - 205 and mouse_y <= (HEIGHT // 2) + int(height) - 235:
+            print('e')
+    # for i in range(len(title_and_location)):
+        #print(locations[i], titles[i], str(mouse_x), str(mouse_y))
     for note in note_titles:
         # i is the amount of space inbetween the different note titles
         i += 40
-        screen.blit(title_font.render(note, True, WHITE),
+        pygame.draw.rect(screen, (245, 245, 220), ((WIDTH // 2) - 125,
+                         (HEIGHT // 2) + i - 205, 205, 30))
+        screen.blit(title_font.render(note, True, BLACK),
                     ((WIDTH // 2) - 120, (HEIGHT // 2) + i - 200))
+
+    # check if mouse is over one of the boxes
 
     pygame.display.flip()
 
@@ -65,5 +115,5 @@ sys.exit()
 
 # # # # # # # # # # # # # # # NEXT STEP: create boxes around the titles and make it so that wehn you hover over the boxes it creates a drop down
 # # # # # # # # # # # # # # # showing the title of the note and everything in it and when you take your mouse off it it close again
-# # # # # # # # # # # # # # # NEXT STEP: make it so that the title would only be the first 20 letters if theres no title which can be defined in the
-# # # # # # # # # # # # # # # data.txt file with (((title here))) you can use .split() to seperate the title and the text
+# # # # # # # # # # # # # # # NEXT STEP: replace notes with ids so that if they have the same title they can be distinguished
+# # # # # # # # # # # # # # #
